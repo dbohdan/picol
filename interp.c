81ce8b9c253cf704616018680aa91f6ef9625111
dbohdan <dbohdan>
2016-01-24 13:38:05 +0000

    interp.c: Put command line arguments in the list variable argv with each argument properly quoted. This lets you handle arguments with whitespace in them. Set argv for '-e' mode as well.

--- interp.c
+++ interp.c
@@ -2,45 +2,63 @@
 
 #include "picol.h"
 
-int main(int argc, char **argv) {
-  picolInterp *i = picolCreateInterp();
-  char buf[MAXSTR] = "";
-  int  rc;
-  FILE* fp = fopen("init.pcl","r");
-  picolSetVar(i,"argv0", argv[0]);
-  picolSetVar(i,"argv",  "");
-  picolSetVar(i,"argc",  "0");
-  picolSetVar(i,"auto_path", "");
-  picolEval(i,"array set env {}"); /* populated from real env on demand */
-  if(fp) {
-    fclose(fp);
-    rc = picolSource(i,"init.pcl");
-    if(rc != PICOL_OK) puts(i->result);
-    i->current = NULL; /* avoid misleading error traceback */
-  }
-  if (argc == 1) {           /* no arguments - interactive mode */
-    while(1) {
-      printf("picol> "); fflush(stdout);
-      if (fgets(buf,sizeof(buf),stdin) == NULL) return 0;
-      rc = picolEval(i,buf);
-      if (i->result[0] != '\0' || rc != PICOL_OK)
-        printf("[%d] %s\n", rc, i->result);
+void set_interp_argv(picolInterp* pi, int offset, int argc, char** argv) {
+    char buf[MAXSTR] = "";
+    int i;
+    picolSetIntVar(pi, "argc", argc - offset);
+    for (i = offset; i < argc; i++) {
+        LAPPEND(buf, argv[i]);
     }
-  } else if(argc==3 && EQ(argv[1],"-e")) {   /* script in argv[2] */
-    rc = picolEval(i,argv[2]);
-    if(rc != PICOL_OK) {
-      picolVar *v = picolGetVar(i,"::errorInfo");
-      if(v) puts(v->val);
-    } else puts(i->result);
-  } else {    /* first arg is file to source, rest goes to argv */
-    picolSetVar(i,   "argv0",argv[1]);
-    picolSetVar(i,   "argv", picolConcat(buf,argc-1,argv+1));
-    picolSetIntVar(i,"argc",argc-2);
-    rc = picolSource(i,argv[1]);
-    if(rc != PICOL_OK) {
-      picolVar *v = picolGetVar(i,"::errorInfo");
-      if(v) puts(v->val);
+    picolSetVar(pi, "argv", buf);
+}
+
+int main(int argc, char** argv) {
+    picolInterp* pi = picolCreateInterp();
+    char buf[MAXSTR] = "";
+    int rc = 0;
+    int i = 0;
+    FILE* fp = fopen("init.pcl", "r");
+    picolSetVar(pi, "argv0", argv[0]);
+    picolSetVar(pi, "argv",  "");
+    picolSetVar(pi, "argc",  "0");
+    picolSetVar(pi, "auto_path", "");
+    picolEval(pi, "array set env {}"); /* populated from real env on demand */
+    if (fp) {
+        fclose(fp);
+        rc = picolSource(pi, "init.pcl");
+        if (rc != PICOL_OK) {
+            puts(pi->result);
+        }
+        pi->current = NULL; /* avoid misleading error traceback */
     }
-  }
-  return rc & 0xFF;
+    if (argc == 1) { /* no arguments - interactive mode */
+        while (1) {
+            printf("picol> ");
+            fflush(stdout);
+            if (fgets(buf, sizeof(buf), stdin) == NULL) return 0;
+            rc = picolEval(pi, buf);
+            if (pi->result[0] != '\0' || rc != PICOL_OK)
+                printf("[%d] %s\n", rc, pi->result);
+        }
+    } else if (EQ(argv[1], "-e")) { /* script in argv[2] */
+        set_interp_argv(pi, 1, argc, argv);
+        rc = picolEval(pi, argv[2]);
+        if (rc != PICOL_OK) {
+            picolVar *v = picolGetVar(pi, "::errorInfo");
+            if (v) {
+                puts(v->val);
+            }
+        } else puts(pi->result);
+    } else { /* first arg is file to source, rest goes to argv */
+        picolSetVar(pi, "argv0", argv[1]);
+        set_interp_argv(pi, 2, argc, argv);
+        rc = picolSource(pi, argv[1]);
+        if (rc != PICOL_OK) {
+            picolVar *v = picolGetVar(pi, "::errorInfo");
+            if (v) {
+                puts(v->val);
+            }
+        }
+    }
+    return rc & 0xFF;
 }
