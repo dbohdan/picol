6bdc7b8f4f639561ae95cc7f96da2eeb3447b1f5
dbohdan <dbohdan>
2016-09-13 20:46:29 +0000

    build-mingw.cmd, build-msvc.cmd: Noninteractive mode.

--- build-mingw.cmd
+++ build-mingw.cmd
@@ -1,7 +1,14 @@
 @echo off
-rem Build Picol on Windows with MinGW.
+
+rem Process the command line arguments.
+set flag_batch=0
+for %%a in (%*) do (
+    if "%%a"=="/batch" set flag_batch=1
+)
+
+rem Build Picol with MinGW.
 set PATH=C:\MinGW\bin;C:\MinGW\msys\1.0\bin
 set CC=gcc
 make
 make examples
-pause
+if "%flag_batch%"=="0" pause
--- build-msvc.cmd
+++ build-msvc.cmd
@@ -1,6 +1,13 @@
 @echo off
-rem Build Picol on Windows with MSVC.
+
+rem Process the command line arguments.
+set flag_batch=0
+for %%a in (%*) do (
+    if "%%a"=="/batch" set flag_batch=1
+)
+
+rem Build Picol with MSVC.
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 echo on
 nmake /F Makefile.nmake
-pause
+if "%flag_batch%"=="0" pause
