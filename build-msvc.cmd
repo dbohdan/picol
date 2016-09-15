d03341a3ce5043e5b230adeb6f9d6444d31b6b32
dbohdan <dbohdan>
2016-09-15 10:04:49 +0000

    build-msvc.cmd: Fix unfinished version committed.

--- build-msvc.cmd
+++ build-msvc.cmd
@@ -1,19 +1,19 @@
-@echo off
+@echo on
 
 setlocal ENABLEDELAYEDEXPANSION
 
 rem Process the command line arguments.
 set flag_batch=0
-set msvc_version=14.0
+set msvs_version=14.0
 for %%a in (%*) do (
-    set flag="%%a"
+    set flag=%%a
     if "!flag!"=="/batch" set flag_batch=1
-    if "!flag:~0,9!"=="/version:" set version="!arg:~7!"
+    if "!flag:~0,9!"=="/version:" set msvs_version=!flag:~9!
 )
 
 rem Build Picol with MSVC.
 echo on
-call "C:\Program Files (x86)\Microsoft Visual Studio %msvc_version%\VC\vcvarsall.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio %msvs_version%\VC\vcvarsall.bat"
 echo on
 nmake /F Makefile.nmake
 @if "%flag_batch%"=="0" pause
