f655c6b781b9b780d4807ad472fda848cd660010
dbohdan <dbohdan>
2016-09-15 09:56:26 +0000

    build-msvc.cmd: Implement a switch for setting the Microsoft Visual Studio version: /version:foo.

--- build-msvc.cmd
+++ build-msvc.cmd
@@ -1,14 +1,19 @@
 @echo off
 
+setlocal ENABLEDELAYEDEXPANSION
+
 rem Process the command line arguments.
 set flag_batch=0
+set msvc_version=14.0
 for %%a in (%*) do (
-    if "%%a"=="/batch" set flag_batch=1
+    set flag="%%a"
+    if "!flag!"=="/batch" set flag_batch=1
+    if "!flag:~0,9!"=="/version:" set version="!arg:~7!"
 )
 
 rem Build Picol with MSVC.
 echo on
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio %msvc_version%\VC\vcvarsall.bat"
 echo on
 nmake /F Makefile.nmake
 @if "%flag_batch%"=="0" pause
